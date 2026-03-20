eb3baae06f4926236c9ad36d18deaf8d0c611d03
Daniil <Daniil@DESKTOP-P1VKMVF>
2026-03-20 08:23:03 +0700

    Добавьте файлы проекта.

---
+++ Figure.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+public abstract class Figure
+{
+    public Color Color { get; set; }
+    public abstract UIElement CreateUIElement(double size = 50);
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            colorComboBox.Items.Add(new ComboBoxItem { Content = "Red" });
+            colorComboBox.Items.Add(new ComboBoxItem { Content = "Green" });
+            colorComboBox.Items.Add(new ComboBoxItem { Content = "Blue" });
+
+            colorComboBox.SelectedIndex = 0;   
+
+            UpdateShapes(); 
+        }
+
+        private void ColorComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateShapes();
+        }
+
+        private void UpdateShapes()
+        {
+            if (shapesPanel == null) return;
+
+            shapesPanel.Children.Clear();
+
+            if (colorComboBox.SelectedItem is ComboBoxItem selectedItem)
+            {
+                string color = selectedItem.Content.ToString();
+
+                CircleCreator circleCreator;
+                SquareCreator squareCreator;
+                TriangleCreator triangleCreator;
+
+                switch (color)
+                {
+                    case "Red":
+                        circleCreator = new RedCircleCreator();
+                        squareCreator = new RedSquareCreator();
+                        triangleCreator = new RedTriangleCreator();
+                        break;
+                    case "Green":
+                        circleCreator = new GreenCircleCreator();
+                        squareCreator = new GreenSquareCreator();
+                        triangleCreator = new GreenTriangleCreator();
+                        break;
+                    case "Blue":
+                        circleCreator = new BlueCircleCreator();
+                        squareCreator = new BlueSquareCreator();
+                        triangleCreator = new BlueTriangleCreator();
+                        break;
+                    default:
+                        return;
+                }
+
+                shapesPanel.Children.Add(circleCreator.CreateCircle().CreateUIElement(80));
+                shapesPanel.Children.Add(squareCreator.CreateSquare().CreateUIElement(80));
+                shapesPanel.Children.Add(triangleCreator.CreateTriangle().CreateUIElement(80));
+            }
+        }
+    }
+}
